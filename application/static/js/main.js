1bdb152499bcefb3cef8e0119a81a382048ab6b3
Matyas Albert Nagy <albertmatyi@gmail.com>
2012-05-27 22:35:40 +0300

    Added loading graphic

--- application/static/js/main.js
+++ application/static/js/main.js
@@ -1,13 +1,35 @@
+var positions=Array();
+var postsPadding = 0;
+
 function gotoPost(id){
-	window.location=(window.location+'').split('#')[0]+'#post'+id;
+	//window.location=(window.location+'').split('#')[0]+'#post'+id;
+	centerDelta = ($('#post-container').width()-$('#post'+id).width()-50)/2;
+	$('#post-container').scrollLeft(positions[id]+postsPadding-centerDelta);
 	$('.pagination li.active').removeClass('active'); 
 	$('.pagination li a[href="#post'+id+'"]').parent().addClass('active');
 	return false;
 }
 
+function initPosts(){
+	positions=Array();
+	els = $('.posts').children();
+	w = 0;
+	for(i=0; i<els.size();i++){
+		positions.push(w);
+		w += $(els[i]).width()+50; //10 lmargin 40borders
+	}
+	$('.posts').width(w);
+}
+
 $(window).load(function() {
-	els = $('#posts')
-	for(int i)
+	initPosts();
+	$('.posts').css('padding-left', $('.span10').width()/4);
+	$('.posts').css('padding-right', $('.span10').width()/4);
+	postsPadding = parseInt($('.posts').css('padding-right'));
+	gotoPost(0);
+	$('.posts').css('position','static');
+	$('.posts').css('top', null);
+	$('#loader').remove();
 //	$('#slider').slider();
 	//TODO try make the custom scrollbar work
 	

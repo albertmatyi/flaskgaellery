6c1b1a782cba361f2c56ed44d7c9c9b6a922910a
Matyas Albert Nagy <albertmatyi@gmail.com>
2012-06-05 22:06:31 +0300

    Changed the styling of the paginator, and added adminrights to sum specific ppl (email addresses)

--- application/decorators.py
+++ application/decorators.py
@@ -8,6 +8,7 @@ Decorators for URL handlers
 from functools import wraps
 from google.appengine.api import users
 from flask import redirect, request
+from flask.helpers import url_for
 
 
 def login_required(func):
@@ -19,13 +20,16 @@ def login_required(func):
         return func(*args, **kwargs)
     return decorated_view
 
-
+administrators = ['albertmatyi@gmail.com', 'anabella.veress@googlemail.com', 'bakosstamas@gmail.com']
 def admin_required(func):
     """Requires App Engine admin credentials"""
     @wraps(func)
     def decorated_view(*args, **kwargs):
-        if not users.is_current_user_admin():
+        usr = users.get_current_user()
+        if not usr:
             return redirect(users.create_login_url(request.url))
+        elif usr.email().lower() not in administrators:
+            return redirect(url_for('home'), 302)
         return func(*args, **kwargs)
     return decorated_view
 
